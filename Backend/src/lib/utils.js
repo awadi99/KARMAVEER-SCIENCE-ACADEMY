822fe492f57608fa434c4f81a8080a3e418cbb23
awadi99 <adityawaghmare9990@gmail.com>
2026-05-20 13:21:33 +0530

    error 22

--- Backend/src/lib/utils.js
+++ Backend/src/lib/utils.js
@@ -13,7 +13,7 @@ export const generateToken = (userData,res)=>{
     res.cookie("jwt",token,{
         httpOnly: true,
         secure: true,
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000
     });
     return token;
