901bb1d7f240dbdc5aae15d8c0b90c8169a3e9e3
awadi99 <adityawaghmare9990@gmail.com>
2026-05-21 16:23:41 +0530

    error 250

--- Frontend/src/constants/facultyData.js
+++ Frontend/src/constants/facultyData.js
@@ -1,12 +1,12 @@
 import { sharadBhosale,sudam,Priya,mulani,salunke,boravake } from "../assets/faculty/index.js";
 
 const FACULTY_DATA = [
-    { name: "Sharad Bhosale", qual: "M.Sc. Mathematics", exp: "12+ Years", expertise: "Mathematics", img: sharadBhosale },
-    { name: "Boravake Sir", qual: "M.Sc. Physics", exp: "8+ Years", expertise: "Physics",img:boravake },
-    { name: "Salunke Sir", qual: "M.Sc. Biology", exp: "7+ Years", expertise: "Biology",img:salunke },
-    { name: "Mulani Sir", qual: "M.Sc. Chemistry", exp: "10+ Years", expertise: "Chemistry",img:mulani },
-    { name: "Sudam Shinde", qual: "", exp: "", expertise: "Management Office", img: sudam },
-    { name: "Priya Yadav", qual: "M.E Heat Power(Mech Engg)", exp: "7+ Years", expertise: "", img: Priya },
+    { name: "Sharad Bhosale", qual: "M.Sc. Mathematics B.Ed", exp: "15+ Years", expertise: "Mathematics", img: sharadBhosale },
+    { name: "Boravake Sir", qual: "M.Sc. Physics B.Ed", exp: "15+ Years", expertise: "Physics",img:boravake },
+    { name: "Salunke Sir", qual: "M.Sc. Zoology B.Ed", exp: "15+ Years", expertise: "Biology",img:salunke },
+    { name: "Mulani Sir", qual: "M.Sc. Chemistry L.L.B", exp: "15+ Years", expertise: "Chemistry",img:mulani },
+    { name: "Sudam Shinde", qual: "", exp: "15+ Years", expertise: "Management Office", img: sudam },
+    { name: "Priya Yadav", qual: "M.E Heat Power(Mech Engg)", exp: "15+ Years", expertise: "", img: Priya },
 ];
 
 export default FACULTY_DATA;
