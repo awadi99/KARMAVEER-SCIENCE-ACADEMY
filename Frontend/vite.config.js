5b0383540a3cd2239d56ee052aefa96b086f6aae
awadi99 <adityawaghmare9990@gmail.com>
2026-05-22 02:02:55 +0530

    update 10

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,25 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   base: "/",
   plugins: [
     react(),
     tailwindcss(),
-    Sitemap({
-      hostname: 'https://karmaveerscienceacademy.in', 
-      outDir: 'dist',
-      routes: [
-        '/',
-        '/about',
-        '/contact',
-        '/Faq',
-        '/login',
-        '/register'
-      ]
-    })
+    // Sitemap plugin yahan se hata diya gaya hai
   ],
   server: {
     proxy: {
